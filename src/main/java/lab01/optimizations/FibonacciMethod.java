5146ce9050b094949735305968d596116ed700bf
Yaroslav <Kirsanov>
2021-02-26 20:33:30 +0300

    Fibonacci method update

--- src/main/java/lab01/optimizations/FibonacciMethod.java
+++ src/main/java/lab01/optimizations/FibonacciMethod.java
@@ -23,10 +23,12 @@ public class FibonacciMethod implements UnaryOptimization {
         List<Iteration> iterations = new ArrayList<>();
         final List<Double> fibonacciNumbers = getFibonacciNumbers(l, r, epsilon);
         final int n = fibonacciNumbers.size() - 2;
+        final double l_0 = l;
+        final double r_0 = r;
 
-        for (int k = 1; k < n; k++) {
-            double x_1 = l + fibonacciNumbers.get(n - k - 1) / fibonacciNumbers.get(n - k + 1) * (r - l);
-            double x_2 = l + fibonacciNumbers.get(n - k) / fibonacciNumbers.get(n - k + 2) * (r - l);
+        for (int k = 1; k <= n; k++) {
+            double x_1 = l + fibonacciNumbers.get(n - k + 1) / fibonacciNumbers.get(n + 2) * (r_0 - l_0);
+            double x_2 = l + fibonacciNumbers.get(n - k + 2) / fibonacciNumbers.get(n + 2) * (r_0 - l_0);
 
             if (formula.apply(x_1) <= formula.apply(x_2)) {
                 r = x_2;
