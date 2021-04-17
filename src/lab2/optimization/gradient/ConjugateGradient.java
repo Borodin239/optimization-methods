65b823e12f0bfbb8e1825c9200174d0a85111e3e
tolisso <tolisso@e.malko@bk.ru>
2021-04-17 18:49:36 +0300

    ConjugateGradient fixed

--- src/lab2/optimization/gradient/ConjugateGradient.java
+++ src/lab2/optimization/gradient/ConjugateGradient.java
@@ -1,6 +1,7 @@
 package lab2.optimization.gradient;
 
 import lab2.optimization.QuadraticForm;
+import org.la4j.Matrix;
 import org.la4j.Vector;
 import org.la4j.vector.dense.BasicVector;
 
@@ -11,33 +12,44 @@ public class ConjugateGradient extends BasicGradient {
     ArrayList<Double> euclideanNormsSquare = new ArrayList<>();
     ArrayList<Vector> ps = new ArrayList<>();
 
+    private double mul(Matrix a, Vector p) {
+        Vector ap = a.multiply(p);
+        double res = 0;
+        for (int i = 0; i < p.length(); i++) {
+            res += ap.get(i) * p.get(i);
+        }
+        return res;
+    }
+
     protected Vector computeNext(QuadraticForm form, Vector x) {
-        double currGradientNorm = form.getGradient(x).euclideanNorm();
-        euclideanNormsSquare.add(currGradientNorm * currGradientNorm);
-        double b = euclideanNormsSquare.get(euclideanNormsSquare.size() - 1) /
-                euclideanNormsSquare.get(euclideanNormsSquare.size() - 2);
-        ps.add(ps.get(ps.size() - 1).multiply(b).subtract(form.getGradient(x)));
-        Vector to = getNextPoint(form, x, ps.get(ps.size() - 1));
-        return Gradient.getMinOnSlice(form, x, to, epsilon);
+        double gradientNormK = euclideanNormsSquare.get(euclideanNormsSquare.size() - 1);
+
+//        Vector to = getNextPoint(form, x, ps.get(ps.size() - 1));
+//        Vector x1 = Gradient.getMinOnSlice(form, x, to, epsilon);
+        Vector pk = ps.get(ps.size() - 1);
+        double ak = gradientNormK / mul(form.getA(), pk);
+        Vector x1 = x.add(pk.multiply(ak));
+
+        double gradientNormK1 = form.getGradient(x1).euclideanNorm();
+        euclideanNormsSquare.add(gradientNormK1 * gradientNormK1);
+        double b = gradientNormK1 / gradientNormK;
+        ps.add(ps.get(ps.size() - 1).multiply(b).subtract(form.getGradient(x1)));
+        return x1;
     }
 
     protected Vector getIteration(QuadraticForm form, Vector x) {
-        if (ps.size() == form.size()) {
+        if (ps.size() == form.size() || ps.isEmpty()) {
             reinit(form, x);
         }
-        if (ps.size() == 0) {
-            reinit(form, x);
-            return x;
-        } else {
-            return computeNext(form, x);
-        }
+        return computeNext(form, x);
     }
 
     private void reinit(QuadraticForm form, Vector x) {
         ps.clear();
         euclideanNormsSquare.clear();
-        euclideanNormsSquare.add(Math.pow(form.getGradient(x).euclideanNorm(), 2));
+
         ps.add(form.getGradient(x).multiply(-1));
+        euclideanNormsSquare.add(Math.pow(form.getGradient(x).euclideanNorm(), 2));
     }
 
     protected Vector getNextPoint(QuadraticForm form, Vector x, Vector p) {
