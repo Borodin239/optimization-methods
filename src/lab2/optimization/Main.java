30d2d2f4afb0d147e0836ff10769b77d9559cc50
tolisso <tolisso@e.malko@bk.ru>
2021-04-27 22:22:19 +0300

    LinearQuadraticForm added

--- src/lab2/optimization/Main.java
+++ src/lab2/optimization/Main.java
@@ -24,12 +24,9 @@ public class Main {
 //                {1, 0},
 //                {0, 4},
 //        }, new double[] {4, 2});
-        QuadraticForm form = new QuadraticForm(new double[][]{
-                {508, 506},
-                {506, 508}
-        }, new double[] {50, 130}, -111);
-        double x = 10, y = 10;
-        Gradient opt = new ConjugateGradient();
+        QuadraticForm form = new LinearQuadraticForm(new double[]{1, 13});
+        double x = 13, y = 123;
+        Gradient opt = new FastestGradient();
         List<Iteration> res = opt.getOptimization(form, 0.001, x, y);
 
         for (int i = 0; i < res.size(); i++) {
