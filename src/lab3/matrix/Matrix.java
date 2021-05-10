cde8ae4ed7079b2570d6aefc588d73ea26664cfa
tolisso <tolisso@e.malko@bk.ru>
2021-05-10 19:44:10 +0300

    lab3: task 1 added

--- src/lab3/matrix/Matrix.java
+++ src/lab3/matrix/Matrix.java
@@ -3,4 +3,5 @@ package lab3.matrix;
 public interface Matrix {
     double get(int i, int j);
     int size();
+    void set(int i, int j, double val);
 }
--- src/lab3/matrix/ProfileMatrix.java
+++ src/lab3/matrix/ProfileMatrix.java
@@ -15,6 +15,26 @@ public class ProfileMatrix implements Matrix {
         buildAu(m, size);
     }
 
+    private boolean isInProfile(int i, int j) {
+        if (i == j) {
+            return true;
+        } else if (i > j) {
+            int start = i - (ial[i + 1] - ial[i]);
+            return j >= start;
+        } else {
+            int start = j - (iau[j + 1] - iau[j]);
+            return i >= start;
+        }
+    }
+
+    private int firstInProfileL(int i) {
+        return i - (ial[i + 1] - ial[i]);
+    }
+
+    private int firstInProfileU(int i) {
+        return i - (iau[i + 1] - iau[i]);
+    }
+
     private void checkSize(double[][] m) {
         for (double[] arr : m) {
             if (m.length != arr.length) {
@@ -96,8 +116,120 @@ public class ProfileMatrix implements Matrix {
         }
     }
 
+    public void set(int i, int j, double val) {
+        if (i == j) {
+            di[i] = val;
+        } else if (i > j) {
+            int start = i - (ial[i + 1] - ial[i]);
+            if (j < start) {
+                throw new IllegalArgumentException("Out of profile");
+            } else {
+                al[ial[i] + j - start] = val;
+            }
+        } else {
+            int start = j - (iau[j + 1] - iau[j]);
+            if (i < start) {
+                throw new IllegalArgumentException("Out of profile");
+            } else {
+                au[iau[j] + i - start] = val;
+            }
+        }
+    }
+
     @Override
     public int size() {
         return di.length;
+    }
+
+    public Matrix[] getLU() {
+        for (int i = 1; i < size(); i++) {
+            for (int j = firstInProfileL(i); j < i; j++) {
+                if (i == 3 && j == 2) {
+                    System.out.println("here");
+                }
+                double tmp = get(i, j);
+                for (int k = 0; k < j; k++) {
+                    tmp -= get(i, k) * get(k, j);
+                }
+                if (tmp != 0) {
+                    tmp /= get(j, j);
+                }
+                set(i, j, tmp);
+            }
+            for (int j = firstInProfileU(i); j < i; j++) {
+                double tmp = get(j, i);
+                for (int k = 0; k < j; k++) {
+                    tmp -= get(j, k) * get(k, i);
+                }
+                set(j, i, tmp);
+            }
+            double tmp = get(i, i);
+            for (int k = 0; k < i; k++) {
+                tmp -= get(i, k) * get(k, i);
+            }
+            set(i, i, tmp);
+        }
+        Matrix L = new Matrix() {
+            @Override
+            public double get(int i, int j) {
+                if (i < j) return 0;
+                if (i == j) return 1;
+                return ProfileMatrix.this.get(i, j);
+            }
+            @Override
+            public int size() {
+                return ProfileMatrix.this.size();
+            }
+            @Override
+            public void set(int i, int j, double val) {
+                throw new IllegalArgumentException();
+            }
+        };
+
+        Matrix U = new Matrix() {
+            @Override
+            public double get(int i, int j) {
+                if (i > j) return 0;
+                return ProfileMatrix.this.get(i, j);
+            }
+            @Override
+            public int size() {
+                return ProfileMatrix.this.size();
+            }
+            @Override
+            public void set(int i, int j, double val) {
+                throw new IllegalArgumentException();
+            }
+        };
+
+        return new Matrix[] {L, U};
+    }
+
+    double[] solveByLU(double[] b) {
+        if (b.length != size()) {
+            throw new IllegalArgumentException("Wrong b argument size");
+        }
+        Matrix[] LU = getLU();
+        Matrix L = LU[0];
+        Matrix U = LU[1];
+
+        double[] y = new double[size()];
+
+        for (int i = 0; i < size(); i++) {
+            y[i] = b[i];
+            for (int j = firstInProfileL(i); j < i; j++) {
+                y[i] -= L.get(i, j) * y[j];
+            }
+        }
+
+        double[] x = new double[size()];
+        for (int i = size() - 1; i > -1; i--) {
+            x[i] = y[i];
+            for (int j = size() - 1; j > i; j--) {
+                x[i] -= U.get(i, j) * x[j];
+            }
+            x[i] /= U.get(i, i);
+        }
+        return x;
     }
 }
