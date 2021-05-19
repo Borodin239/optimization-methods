800c34613f0d6dd593b7504a654d879cd5c9fb8e
Евгений Бородин <evgeniiborodin@MacBook-Air-Evgenij-2.local>
2021-05-19 15:11:46 +0300

    CRSMatrix was added

---
+++ src/lab3/CRSMatrix.java
@@ -0,0 +1,21 @@
+package lab3;
+
+/**
+ * Compressed Row Storage Matrix realisation.
+ */
+public class CRSMatrix implements Matrix {
+    @Override
+    public double get(int i, int j) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void set(int i, int j, double val) {
+
+    }
+}
